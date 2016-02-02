d67e0f5a5f040eb1fcc4e15522537fd8866f35ec
unknown <Nagisachan>
2016-02-02 16:48:02 +0700

    add beta1 round robin and input
    add beta1 round robin, need to rearrange and comment but this code can
    run properly.

--- round roubin.c
+++ round roubin.c
@@ -1,21 +1,42 @@
-#include<stdio.h>
-int main()
-{
-  int i,j=0,n,time,remain,flag=0,ts;
-  int sum_wait=0,sum_turnaround=0,at[10],bt[10],rt[10];
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+  int numberOfJob;
+  int *noJob;
+  int *timeArrive;
+  int *timeUse;
+  int *rt;
+  
+  FILE* file = fopen (argv[1], "r");
+  int i = 0;
+
+  fscanf (file, "%d", &numberOfJob);  
+  noJob = (int*)malloc(numberOfJob*sizeof(int));
+  timeArrive = (int*)malloc(numberOfJob*sizeof(int));
+  timeUse = (int*)malloc(numberOfJob*sizeof(int));
+  rt = (int*)malloc(numberOfJob*sizeof(int));
+  for(i=0;i<numberOfJob;i++){
+	fscanf(file,"%d %d %d",&noJob[i],&timeArrive[i],&timeUse[i]);
+  }
+  
+  printf("No of Job = %d\n",numberOfJob);
+
+  for(i=0;i<numberOfJob;i++){
+	printf("%d %d %d\n",noJob[i],timeArrive[i],timeUse[i]);
+  }
+  
+
+  
+  int j=0,n = numberOfJob,time,remain = numberOfJob,flag=0;
+  int ts = 2; //time slice
+  int sum_wait=0,sum_turnaround=0; //at = arrive time, bt = burst time, rt = current time in each node
   int ganttP[50],ganttStartTime[50];
-  printf("Enter no of Processes : ");
-  scanf("%d",&n);
-  remain=n;
+  
   for(i=0;i<n;i++)
   {
-    printf("Enter arrival time and burst time for Process P%d :",i+1);
-    scanf("%d",&at[i]);
-    scanf("%d",&bt[i]);
-    rt[i]=bt[i];
+    rt[i]=timeUse[i];
   }
-  printf("Enter time slice");
-  scanf("%d",&ts);
   printf("\n\nProcess\t|Turnaround time|waiting time\n\n");
   for(time=0,i=0;remain!=0;)
   {
@@ -37,14 +58,14 @@ int main()
     if(rt[i]==0 && flag==1)
     {
       remain--;
-      printf("P[%d]\t|\t%d\t|\t%d\n",i+1,time-at[i],time-at[i]-bt[i]);
-      sum_wait+=time-at[i]-bt[i];
-      sum_turnaround+=time-at[i];
+      printf("P[%d]\t|\t%d\t|\t%d\n",i+1,time-timeArrive[i],time-timeArrive[i]-timeUse[i]);
+      sum_wait+=time-timeArrive[i]-timeUse[i];
+      sum_turnaround+=time-timeArrive[i];
       flag=0;
     }
     if(i==n-1)
       i=0;
-    else if(at[i+1]<=time)
+    else if(timeArrive[i+1]<=time)
       i++;
   }
   printf("\nAvg sum_wait = %f\n",sum_wait*1.0/n);
@@ -54,5 +75,6 @@ int main()
   {
       printf("(P[%d],%d) ",ganttP[i],ganttStartTime[i]);
   }
-  return 0;
+  printf("\n");
+  fclose (file);   
 }
